945d83b26615fa225a58d8a641aabe14c3f08302
jx0808 <fonteynejens@gmail.com>
2013-11-01 22:22:39 +0100

    adjusted site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,30 @@ node default
 	ensure => installed,
 	}
 
+	package 
+	{ 'mod_ssl':
+	ensure => installed,
+	}
+
+	package 
+	{ 'mysql':
+	ensure => installed,
+	}
+
+	package 
+	{ 'mysql-server':
+	ensure => installed,
+	}
+
+
+
 	service
 	{ 'httpd':
+	ensure => running,
+	}
+
+	service
+	{ 'mysqld':
 	ensure => running,
 	}
 }
